ebcc19078685b8c6b7d78c581964a8d2905c510d
chriss1525 <okothchris15@gmail.com>
2023-01-28 12:14:32 +0300

    factorizing big numbers?

--- factors.c
+++ factors.c
@@ -1,22 +1,28 @@
 #include "main.h"
-
 /**
  * factors - breaks numbers into factorials
  * Return: 0 (success)
  */
-int factors(mytypedef_t n)
+int factors(unsigned long long n)
 {
-	mytypedef_t w = n / 2;
-	mytypedef_t p;
-	mytypedef_t q;
+	unsigned long long w = n / 2;
+	unsigned long long p;
+	unsigned long long q;
 
-	for (q = 2; q <= w; q++)
+	if (n % q == 0)
 	{
-		if (n % q == 0)
+		for (q = 2; q <= w; q++)
 		{
-			p = n / q;
-			break;
+			{
+				p = n / q;
+				break;
+			}
 		}
+	}
+	else
+	{
+		q = n;
+		p = n / n;
 	}
 	printf("%llu=%llu*%llu\n", n, p, q);
 	return (0);
